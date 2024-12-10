bf59f40bb6a9131171daec65426027f9a0a2f8ea
ValentinT90 <Valentos90@Gmail.com>
2024-12-10 15:45:07 +0900

    Update module_1_4.py

--- module_1_4.py
+++ module_1_4.py
@@ -1,5 +1,5 @@
 homeWork=12
 hour=1.5
-name='Python'
+name='Python,'
 time=(hour*60)/homeWork/60
-print('Курс:',name,"всего задач:",homeWork,'Затрачено часов:',hour,'среднее время выполнения:',time)
+print('Курс:',name,"всего задач:",homeWork,', ''Затрачено часов:',hour,', ''среднее время выполнения:',time)
