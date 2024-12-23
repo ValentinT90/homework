821f81378acdcc8aee48a609a2161500525811c7
Валентин <Valentos90@gmail.com>
2024-12-23 15:50:36 +0900

    На проверку

---
+++ module_2_2.py
@@ -0,0 +1,13 @@
+first = 151
+second = 159
+third = 153
+# print(int(input('Количество одинаковых чисел среди 3-х введёных: ')))
+if first == second and second == third:
+    print(int(3))
+elif first == second or second == third:
+    print(int(2))
+else:
+    first
+not (second and third)
+print(int(0))
+
